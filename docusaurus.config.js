187fdc9bd3284e109322863ffaa8f99781d01eed
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-29 11:50:48 +0900

    To prevent error, rename `themeConfig.navbar.links` to `themeConfig.navbar.items`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
         alt: 'vividarmy-wikinotes Logo',
         src: 'img/logo.svg',
       },
-      links: [
+      items: [
         {
           to: 'docs/doc1',
           activeBasePath: 'docs',
