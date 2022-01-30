d70a9c7691ab1d7c2ba079d308e3d878cf411441
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-30 20:42:49 +0900

    Create sandbox.js

---
+++ src/pages/sandbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classnames from 'classnames';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import SimpleMDE from 'simplemde'
+import 'simplemde/dist/simplemde.min.css'
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    const simplemde = new SimpleMDE()
+  );
+}
+
+export default SandBox;
