a53a2d7e0c1ddc0425c153aea98a93e993edb633
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-01 21:17:32 +0900

    Update sandbox.js

--- src/pages/sandbox.js
+++ src/pages/sandbox.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import classnames from 'classnames';
 // import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import SimpleMDE from 'simplemde'
+// import SimpleMDE from 'simplemde'
 // import 'simplemde/dist/simplemde.min.css'
+import EasyMDE from 'easyMDE';
 
 function Home() {
-  const simplemde = new SimpleMDE()
+  // const simplemde = new SimpleMDE()
+  var easyMDE = new EasyMDE();
 }
 
 export default SandBox;
