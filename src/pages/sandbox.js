ee35b55357831fb075d9be676a9d809aaa589d1e
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-01 21:08:46 +0900

    Update sandbox.js

--- src/pages/sandbox.js
+++ src/pages/sandbox.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import classnames from 'classnames';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// import classnames from 'classnames';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import SimpleMDE from 'simplemde'
 import 'simplemde/dist/simplemde.min.css'
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
   const simplemde = new SimpleMDE()
-  return (
-  );
 }
 
 export default SandBox;
