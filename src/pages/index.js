b504d90ba2376dd8c68dc6885f006643a7709b63
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-18 17:03:51 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,81 @@ import styles from './styles.module.css';
 
 const features = [
   {
+    title: <>Q & A</>,
+    link: 'q-and-a',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        他にはない、SNSのタイムラインのようにサクサク見られるWiki<br \>筆者たちが日々記事を更新しています
+      </>
+    ),
+  },
+  {
+    title: <>個人</>,
+    link: 'personal',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        主に基地内のことについて説明しています<br \>基地内施設やレベル上げなど個人に関するページはここから
+      </>
+    ),
+  },
+  {
+    title: <>連盟</>,
+    link: 'union',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        連盟に関することについて説明しています<br \>連盟施設や遺跡など連盟に関するページはここから
+      </>
+    ),
+  },
+  {
+    title: <>システム</>,
+    link: 'system',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        ゲームのシステムについて説明しています<br \>イベントや英雄などシステムに関するページはここから
+      </>
+    ),
+  },
+  {
+    title: <>サーバー</>,
+    link: 'server',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        サーバーに関することについて説明しています<br \>サーバーや連盟などサーバーに関するページはここから
+      </>
+    ),
+  },
+  {
+    title: <>ヘルプ</>,
+    link: 'help',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        とりあえず迷ったらここ!<br \>わからないことがあったらこちらを参照してみてください
+      </>
+    ),
+  },
+  {
+    title: <>編集後記</>,
+    link: 'blog',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        他にはない、SNSのタイムラインのような編集後記<br \>筆者たちが日々まったり呟いて言います<br \>もしかしたら他のページよりも有益な情報が得られるかも...
+      </>
+    ),
   },
 ];
 
 function Feature({imageUrl, title, description}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
+    <a href={link}>
     <div className={classnames('col col--4', styles.feature)}>
       {imgUrl && (
         <div className="text--center">
@@ -23,6 +92,7 @@ function Feature({imageUrl, title, description}) {
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
+    </a>
   );
 }
 
@@ -31,12 +101,12 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`トップ`}
+      description="WikiNotesであなたの好きをもっと探索しよう">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <p className="hero__subtitle">Wikiなのにちょっと抜けている<br \>Noteなのにちょっと本気<br \>WikiNotesを探索しよう!</p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
