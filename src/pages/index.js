23d308e9b96a099adec37e90da9039a8c5096185
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-20 09:55:52 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,14 +14,7 @@ const features = [
     description: 
       <>
         他にはない、SNSのタイムラインのようにサクサク見られるWiki<br/>筆者たちが日々記事を更新しています
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>      </>
+      </>
   },
   {
     title: <>個人</>,
