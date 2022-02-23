aa56402b2531fd1097b12ffdffa6e2d970f50fa8
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-23 18:40:03 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,13 +150,6 @@ module.exports = {
         sidebarPath: require.resolve('./personal/sidebars.js'),
         showLastUpdateAuthor: true,
         showLastUpdateTime: true,
-        lastVersion: 'wiki',
-        versions: {
-          wiki: {
-            label: 'wiki',
-            path: 'wiki',
-          },
-        },
       },
     ],
     [
